508f17f48eb8a4dfd706c689dc4db9280799e328
PRANATHI83 <apranu8@gmail.com>
2025-07-03 20:36:41 +0530

    updated

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    profile_picture_path VARCHAR(255),
+    profile_picture TEXT, 
     
     -- Account status fields
     is_active BOOLEAN DEFAULT TRUE,
